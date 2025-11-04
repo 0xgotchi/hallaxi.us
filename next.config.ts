10823bcadc7b66b48f29c5b35ca8b24579f27153
u/Horror_Amphibian7516 <nuitsukera@vk.com>
2025-11-04 18:59:58 -0300

    0.4.1

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,21 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@aws-sdk/client-s3"],
+  serverExternalPackages: [
+    "@aws-sdk/client-s3",
+    "@aws-sdk/lib-storage",
+    "@prisma/client",
+  ],
+
   turbopack: {
     root: path.resolve(__dirname),
+  },
+
+  output: "standalone",
+
+  webpack(config) {
+    config.resolve.modules.push(path.resolve(__dirname, "src"));
+    return config;
   },
 };
 
